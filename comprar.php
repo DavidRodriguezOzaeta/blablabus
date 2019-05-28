ba067a71c9a8d4e1dce75a9c25d5026aa41c0fcf
JuanPardos <juanpavh97@gmail.com>
2019-05-28 11:30:28 +0200

    Añadido comprar.php (esta vez si)
    Signed-off-by: JuanPardos <juanpavh97@gmail.com>

---
+++ comprar.php
@@ -0,0 +1,10 @@
+<?php
+	include("conexion_bd.php");
+	
+	$sql="UPDATE viajes SET plazasocupadas = (plazasocupadas + $_GET[cant]) WHERE idviaje = $_GET[id]";   
+	mysqli_query($conexion,$sql) or die("Error en la consulta $sql");
+	mysqli_close($conexion);
+	header("location:buscar.php");
+
+?>
+
